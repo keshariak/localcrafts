1c25bf65c13802b9f3ae7da777f7edb3e55994d9
keshariak <aryankeshari2003@gmail.com>
2023-09-25 01:21:44 +0530

    aryan

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -102,3 +102,4 @@ main.addEventListener("mousemove" , function(dets){
     cursor.style.top=dets.y+(-15)+"px"
     cursor.style.left=dets.x +(-15) +"px"
 })
+
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,14 @@ router.get('/', function(req, res, next) {
 router.get('/product', function(req, res, next) {
   res.render('productpage', { title: 'Express' });
 });
+router.get('/loginpage', function(req, res, next) {
+  res.render('loginpage', { title: 'Express' });
+});
+router.get('/registerpage', function(req, res, next) {
+  res.render('registerpage', { title: 'Express' });
+});
+
+
 
 
 module.exports = router;
